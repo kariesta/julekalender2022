8b28f0787f51f961a2047179d73545e55eb80e9b
kariesta <kariesta@ifi.uio.no>
2021-12-02 11:50:59 +0100

    meep

--- src/components/QrLuke.tsx
+++ src/components/QrLuke.tsx
@@ -17,7 +17,7 @@ function getImage(num: number, pers: String){
     //combine number and name start to image path.
     //const personCodes:  { [key: string]: string; } = {'Kristina':"kris",'Ingrid':"ing"};
     const personCodes = [{name: "Kristina", sufix: "kris"},{name: "Ingrid", sufix: "ing"},{name: "Sigrunn", sufix: "sig"},{name: "ThaoOgTine", sufix: "tt"}]
-    const personCode = personCodes.filter(elem => elem.name == pers)[0].sufix;
+    const personCode = personCodes.filter(elem => elem.name === pers)[0].sufix;
 
     return  "https://kariesta.github.io/Julekalender2021/LUKER/luke"+num.toString()+personCode+".png";
     
